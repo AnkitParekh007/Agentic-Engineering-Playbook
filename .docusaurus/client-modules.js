bdd6b0aedcc01bebea6a6e38d3222c316a0fcc84
Akki Parekh <aparekh@bluemeteor.com>
2026-05-09 21:41:46 +0200

    cleanup commit

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("C:\\Users\\aparekh\\Desktop\\Projects\\akki\\Agentic-Engineering-Playbook\\node_modules\\infima\\dist\\css\\default\\default.css"),
-  require("C:\\Users\\aparekh\\Desktop\\Projects\\akki\\Agentic-Engineering-Playbook\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
-  require("C:\\Users\\aparekh\\Desktop\\Projects\\akki\\Agentic-Engineering-Playbook\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress"),
-  require("C:\\Users\\aparekh\\Desktop\\Projects\\akki\\Agentic-Engineering-Playbook\\src\\css\\custom.css"),
-];
